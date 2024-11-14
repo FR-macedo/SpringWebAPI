cb551c93e53afd4aa6dac41ba60edcb87d130305
FR-macedo <macedo270701@gmail.com>
2024-11-14 00:07:04 -0300

    inicio

---
+++ src/test/java/com/michkin/CursoSpringAPI/CursoSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michkin.CursoSpringAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
